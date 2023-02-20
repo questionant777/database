c2ad4506a167d60dc9e9ec566e4e61c104f1b652
questionant777 <119503924+questionant777@users.noreply.github.com>
2023-02-20 20:35:46 +0300

    Delete data.sql

--- src/main/resources/data.sql
+++
@@ -1,3 +0,0 @@
-insert into author (id, `name`) values (default, 'Kristi');
-insert into genre (id, `name`) values (default, 'detective');
-insert into book (id, `name`, authorid, genreid) values (default, 'Puaro', 1, 1);
